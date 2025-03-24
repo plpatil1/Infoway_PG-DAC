036f11e004b623e5e3da4c3b9a1a2928698ebf91
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-03-24 22:34:17 +0530

    Patterns6,7,8

---
+++ c++/patterns/p7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j, n=5;
+    for(i=1; i<=n; i++)
+    {
+        for(j=n; j>=1; j--)
+        {
+            cout<<j;
+        }
+
+        cout<<"\n";
+    }
+    
+}
+
+
---
+++ c++/patterns/p8.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j, n=5;
+    for(i=n; i>=1; i--)
+    {
+        for(j=n; j>=1; j--)
+        {
+            cout<<i;
+        }
+
+        cout<<"\n";
+    }
+    
+}
+
