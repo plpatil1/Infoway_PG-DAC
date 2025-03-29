7dfc5677bf6ce19a8c38fb167ae6bcad243ebaf0
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-03-29 05:40:33 +0530

    Update pascalTriangle.cpp

--- c++/imp/pascalTriangle.cpp
+++ c++/imp/pascalTriangle.cpp
@@ -1,24 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-int main()
-{
-    int i, j, n;
-    n=5;
-    int m;
-    for(i=0; i<n; i++)
-    {
-        int m=1;
-        for(j=1; j<n-i; j++)
-        {
-            cout<<" ";
+
+int main() {
+    int n = 11, m;
+
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces to center-align the triangle
+        cout << setw((n - i) * 2) << " ";
+
+        m = 1;
+        for (int j = 0; j <= i; j++) {
+            cout << setw(4) << m; // Proper spacing for numbers
+            m = m * (i - j) / (j + 1); // Pascal's Triangle formula
         }
-        for(j=0; j<=i; j++)
-        {
-            cout<<m<<" ";
-            m=m*(i-j)/(j+1);
-        }
-        cout<<"\n";
+        cout << "\n";
     }
-    cout<<"\n THE END....!";
+
     return 0;
 }
