7fb83cca2909b42cc55191e969432fc12388af89
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-08 18:02:36 +0530

    Create Taxable.java

---
+++ DSA_Java/core_Java/Provided/Day05/question/Taxable.java
@@ -0,0 +1,15 @@
+/*3.   Create Interface Taxable with members salesTax=7% and incomeTax=10.5%. create abstract method calcTax().
+a. Create class Employee(empId,name,salary) and implement Taxable to calculate incomeTax on yearly salary.
+b. Create class Product(pid,price,quantity) and implement Taxable to calculate salesTax on unit price of product.
+c. Create class for main method(Say XYZ), accept employee information 
+and a product information from user and print income tax and sales tax respectively.*/
+
+package question2;
+
+public interface Taxable {
+	static float salesTax=7;
+	static float incomeTax=10.5f;
+	
+	public  abstract void calcTax();
+	
+}
