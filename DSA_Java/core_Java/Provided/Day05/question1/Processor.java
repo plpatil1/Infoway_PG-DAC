19ad21a7244f5c59ec22e4b860e7aaa1311442c2
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-08 17:59:50 +0530

    Create Processor.java

---
+++ DSA_Java/core_Java/Provided/Day05/question1/Processor.java
@@ -0,0 +1,31 @@
+/*2. Create an Abstract class Processor with int member variable data  and method showData to display data value.
+ *  Create abstract method process() to define processing of member data. 
+a. Create a class  Factorial using abstract class Processor  to calculate and print factorial of a number by overriding the process method.
+b. Create a class  Circle using abstract class Processor  to calculate and print area of a circle by overriding the process method
+ Ask user to enter choice (factorial or circle area).  Also ask data to work upon; Use Processor class reference to achieve this mechanism*/
+package question1;
+
+public abstract class Processor {
+	protected int data;
+
+	public Processor() {
+		super();
+	}
+
+	public Processor(int data) {
+		
+		this.data = data;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Processor [data=" + data + "]";
+	}
+
+	public abstract void show();
+	
+	
+
+}
