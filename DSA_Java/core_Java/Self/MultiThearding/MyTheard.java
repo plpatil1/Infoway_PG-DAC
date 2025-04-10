a13141dfb0c860de4cb76e5ca13d1232c4f33673
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-10 17:38:52 +0530

    Create MyTheard.java

---
+++ DSA_Java/core_Java/Self/MultiThearding/MyTheard.java
@@ -0,0 +1,16 @@
+package multithreading;
+
+public class MyThread extends Thread{
+	public void run()
+	{
+		System.out.println("Current Thread= "+Thread.currentThread().getName());
+	}
+	public static void main(String[] args) {
+		MyThread t1=new MyThread();
+		MyThread t2=new MyThread();
+		t1.start();
+		t2.start();
+		
+	}
+	
+}
